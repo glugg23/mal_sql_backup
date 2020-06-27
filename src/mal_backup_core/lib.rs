ec9151a2729ecc1a94c0d579b1515092a0f261e5
Max Leonhardt <m.leonhardt424@gmail.com>
2020-06-27 15:14:31 +0100

    Use constants to remove string duplication

--- src/mal_backup_core/lib.rs
+++ src/mal_backup_core/lib.rs
@@ -8,6 +8,8 @@ use scraper::{Html, Selector};
 pub mod chapter;
 pub mod episode;
 
+const URL: &'static str = "https://myanimelist.net/ajaxtb.php";
+const SESSION_COOKIE: &'static str = "is_logged_in=1;MALSESSIONID=";
 const SELECTOR: &'static str = ".spaceit_pad";
 
 pub fn get_anime_episodes(
@@ -16,14 +18,8 @@ pub fn get_anime_episodes(
     client: &Client,
 ) -> Result<Vec<Episode>, Error> {
     let res = client
-        .get(
-            format!(
-                "https://myanimelist.net/ajaxtb.php?detailedaid={}",
-                anime_id
-            )
-            .as_str(),
-        )
-        .header(COOKIE, format!("MALSESSIONID={};is_logged_in=1", session))
+        .get(format!("{}?detailedaid={}", URL, anime_id).as_str())
+        .header(COOKIE, format!("{}{}", SESSION_COOKIE, session))
         .send()?;
 
     let html = Html::parse_document(res.text()?.as_str());
@@ -41,14 +37,8 @@ pub fn get_manga_chapters(
     client: &Client,
 ) -> Result<Vec<Chapter>, Error> {
     let res = client
-        .get(
-            format!(
-                "https://myanimelist.net/ajaxtb.php?detailedmid={}",
-                manga_id
-            )
-            .as_str(),
-        )
-        .header(COOKIE, format!("MALSESSIONID={};is_logged_in=1", session))
+        .get(format!("{}?detailedmid={}", URL, manga_id).as_str())
+        .header(COOKIE, format!("{}{}", SESSION_COOKIE, session))
         .send()?;
 
     let html = Html::parse_document(res.text()?.as_str());
