faef2b2ae21697b4a612bc00ef0bee24efc63516
Max Leonhardt <m.leonhardt424@gmail.com>
2020-07-16 23:20:50 +0100

    Add error logging Log errors to log file Log info to console

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+#[macro_use]
+extern crate log;
+
 use std::env;
+use std::fs::File;
 use std::process::exit;
 
 use clap::{App, Arg};
 use diesel::{Connection, ConnectionError, SqliteConnection};
+use diesel_migrations::run_pending_migrations;
 use dotenv::dotenv;
 use reqwest::blocking::Client;
+use simplelog::*;
 
 use mal_sql_backup::{
     get_anime_episodes, get_anime_list, get_manga_chapters, get_manga_list, get_user_stats,
@@ -37,44 +43,95 @@ fn main() {
         .arg(Arg::with_name("skip-planned"))
         .get_matches();
 
+    CombinedLogger::init(vec![
+        TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed),
+        WriteLogger::new(
+            LevelFilter::Warn,
+            Config::default(),
+            File::create("mal.log").unwrap(),
+        ),
+    ])
+        .unwrap();
+
     let username = args.value_of("username").unwrap();
     let password = args.value_of("password").unwrap();
     let skip_planned = args.is_present("skip-planned");
 
-    let connection = get_db_connection().unwrap_or_else(|_| exit(1));
+    let connection = get_db_connection().unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
+
+    run_pending_migrations(&connection).unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
 
     let client = Client::builder().cookie_store(true).build().unwrap();
 
-    set_session_cookie(&client, username, password).unwrap_or_else(|_| exit(1));
+    set_session_cookie(&client, username, password).unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
 
-    let user = get_user_stats(username, &client).unwrap_or_else(|_| exit(1));
+    let user = get_user_stats(username, &client).unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
 
-    user.save(&connection).unwrap_or_else(|_| exit(1));
+    user.save(&connection).unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
 
-    let anime_list = get_anime_list(&user, &client).unwrap_or_else(|_| exit(1));
+    let anime_list = get_anime_list(&user, &client).unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
 
     for a in anime_list.iter() {
-        a.save(&connection).unwrap_or_else(|_| exit(1));
+        a.save(&connection).unwrap_or_else(|e| {
+            error!("{}", e);
+            exit(1);
+        });
 
         if !(skip_planned && a.watching_status == 6) {
-            let episodes = get_anime_episodes(a.mal_id, &client).unwrap_or_else(|_| exit(1));
+            let episodes = get_anime_episodes(a.mal_id, &client).unwrap_or_else(|e| {
+                error!("{}", e);
+                exit(1);
+            });
 
             episodes.iter().for_each(|e| {
-                e.save(&connection).unwrap_or_else(|_| exit(1));
+                e.save(&connection).unwrap_or_else(|e| {
+                    error!("{}", e);
+                    exit(1);
+                });
             });
         }
     }
 
-    let manga_list = get_manga_list(&user, &client).unwrap_or_else(|_| exit(1));
+    let manga_list = get_manga_list(&user, &client).unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
 
     for m in manga_list.iter() {
-        m.save(&connection).unwrap_or_else(|_| exit(1));
+        m.save(&connection).unwrap_or_else(|e| {
+            error!("{}", e);
+            exit(1);
+        });
 
         if !(skip_planned && m.reading_status == 6) {
-            let chapters = get_manga_chapters(m.mal_id, &client).unwrap();
+            let chapters = get_manga_chapters(m.mal_id, &client).unwrap_or_else(|e| {
+                error!("{}", e);
+                exit(1);
+            });
 
             chapters.iter().for_each(|c| {
-                c.save(&connection).unwrap_or_else(|_| exit(1));
+                c.save(&connection).unwrap_or_else(|e| {
+                    error!("{}", e);
+                    exit(1);
+                });
             });
         }
     }
