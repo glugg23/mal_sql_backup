e83d44f5c8f50282cac098fcd7ccd24d3463ad63
Max Leonhardt <m.leonhardt424@gmail.com>
2020-08-05 17:26:39 +0100

    Update anime_xml view to use details table

--- migrations/2020-07-30-175421_anime_xml_view/up.sql
+++ migrations/2020-07-30-175421_anime_xml_view/up.sql
@@ -1,8 +1,8 @@
 CREATE VIEW anime_xml (mal_id, xml)
 AS
-SELECT mal_id,
+SELECT a.mal_id,
 '<anime>
-    <series_animedb_id>' || mal_id || '</series_animedb_id>
+    <series_animedb_id>' || a.mal_id || '</series_animedb_id>
     <series_title><![CDATA[' || title || ']]></series_title>
     <series_type>' || anime_type || '</series_type>
     <series_episodes>' || total_episodes || '</series_episodes>
@@ -13,15 +13,18 @@ SELECT mal_id,
     <my_rated></my_rated>
     <my_score>' || score || '</my_score>
     <my_dvd></my_dvd>
-    <my_storage></my_storage>
+    <my_storage>' || COALESCE(store.description, '') || '</my_storage>
     <my_status>' || anime_status || '</my_status>
-    <my_comments><![CDATA[]]></my_comments>
-    <my_times_watched>0</my_times_watched>
-    <my_rewatch_value></my_rewatch_value>
+    <my_comments><![CDATA[' || COALESCE(comments, '') || ']]></my_comments>
+    <my_times_watched>' || COALESCE(times_rewatched, 0) || '</my_times_watched>
+    <my_rewatch_value>' || COALESCE(rv.description, '') || '</my_rewatch_value>
     <my_tags><![CDATA[' || COALESCE(tags, '') || ']]></my_tags>
     <my_rewatching>' || is_rewatching || '</my_rewatching>
     <my_rewatching_ep>0</my_rewatching_ep>
     <update_on_import>1</update_on_import>
 </anime>'
-FROM anime
-JOIN statuses ON watching_status = id;
+FROM anime a
+LEFT JOIN anime_details ad ON a.mal_id = ad.mal_id
+LEFT JOIN anime_storage store ON ad.storage = store.id
+LEFT JOIN repeat_value rv on ad.rewatch_value = rv.id
+JOIN statuses stats ON a.watching_status = stats.id;
