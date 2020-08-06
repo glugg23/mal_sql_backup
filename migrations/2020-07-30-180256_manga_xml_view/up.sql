2d4461468626bc096229c13e631e6b77d4e9a998
Max Leonhardt <m.leonhardt424@gmail.com>
2020-08-06 11:05:25 +0100

    Update manga_xml view to use new fields Lots of the new fields are not even used by the official export. Seems like a bit of a waste so I might add them to the output anyway.

--- migrations/2020-07-30-180256_manga_xml_view/up.sql
+++ migrations/2020-07-30-180256_manga_xml_view/up.sql
@@ -1,8 +1,8 @@
 CREATE VIEW manga_xml (mal_id, xml)
 AS
-SELECT mal_id,
+SELECT m.mal_id,
 '<manga>
-    <manga_mangadb_id>' || mal_id || '</manga_mangadb_id>
+    <manga_mangadb_id>' || m.mal_id || '</manga_mangadb_id>
     <manga_title><![CDATA[' || title || ']]></manga_title>
     <manga_volumes>' || total_volumes || '</manga_volumes>
     <manga_chapters>' || total_chapters || '</manga_chapters>
@@ -16,10 +16,12 @@ SELECT mal_id,
     <my_storage></my_storage>
     <my_status>' || manga_status || '</my_status>
     <my_comments><![CDATA[]]></my_comments>
-    <my_times_read>0</my_times_read>
+    <my_times_read>' || md.times_reread || '</my_times_read>
     <my_tags><![CDATA[' || COALESCE(tags, '') || ']]></my_tags>
-    <my_reread_value></my_reread_value>
+    <my_reread_value>' || COALESCE(rv.description, '') || '</my_reread_value>
     <update_on_import>1</update_on_import>
 </manga>'
-FROM manga
-JOIN statuses ON reading_status = id;
+FROM manga m
+LEFT JOIN manga_details md on m.mal_id = md.mal_id
+LEFT JOIN repeat_value rv on md.reread_value = rv.id
+JOIN statuses status ON reading_status = status.id;
